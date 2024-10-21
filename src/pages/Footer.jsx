e79163e107e8bf32b6e55669284c811ee14590de
Jimena <jimenaespinoza@gmail.com>
2024-10-21 19:06:35 -0300

    Centrar footer con className "texto" y css

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -5,9 +5,9 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div className="footer">
-            <h4> Autor: Jimena Espinoza Briceño. Página creada solo para fines académicos</h4>
+            <h4 className="texto"> Autor: Jimena Espinoza Briceño. Página creada solo para fines académicos</h4>
             <hr />
-            <h5>Todos los Derechos reservados - 2024</h5>
+            <h5 className="texto">Todos los Derechos reservados - 2024</h5>
         </div>
     )
 }
